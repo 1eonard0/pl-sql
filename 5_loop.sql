bbaccc1da328a535759ae458f9cc4818204daca2
LToledo <leonardotoledo03@gmail.com>
2024-12-17 14:10:31 -0300

    renames and new file added for loop and labels.

---
+++ 5_loop.sql
@@ -0,0 +1,13 @@
+-- loop and labels
+DECLARE
+	i number(1);
+	j number(1);
+BEGIN
+	<< outer_loop >>
+	FOR i IN 1..3 LOOP
+		<< inner_loop >>
+		FOR j IN 1..3 LOOP
+			dbms_output.put_line('i is: ' || i || ' and j is : ' || j);
+		END LOOP inner_loop;
+	END LOOP outer_loop;
+END;
